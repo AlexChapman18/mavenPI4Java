9f96dda47d7d276d2018abdfc9b86d58e00f815b
Alexander Chapman <achapman5@sheffield.ac.uk>
2022-03-21 15:03:52 +0000

    First push

---
+++ src/main/java/Controller/Run.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import com.pi4j.io.gpio.*;
+
+public class Run {
+    public static void main(String[] args) throws InterruptedException {
+        GpioController gpio = GpioFactory.getInstance();
+        GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+
+        while(true){
+            System.out.println("Toggling LED");
+            pin.toggle();
+            Thread.sleep(1000);
+        }
+    }
+
+}
