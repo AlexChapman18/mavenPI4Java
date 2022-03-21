8e41689cd8294b19e0a142fa3cd098ee0e45bf3e
Alexander Chapman <achapman5@sheffield.ac.uk>
2022-03-21 18:09:09 +0000

    Working blinky blink and starting radio recieve

--- src/main/java/Controller/Run.java
+++ src/main/java/Controller/Run.java
@@ -13,5 +13,4 @@ public class Run {
             Thread.sleep(1000);
         }
     }
-
 }
---
+++ src/main/java/Controller/RunRecieve.java
@@ -0,0 +1,33 @@
+package Controller;
+
+import com.pi4j.io.gpio.*;
+import com.pi4j.wiringpi.Spi;
+
+//
+// Power up prim rx have to set to high (bits in register)
+// CE = Antenna mode (high = recieve) pin: 25
+// SCK = Clock (Ignore? cause sinking clock)
+// MISO = Data output (from antenna to pi) pin:
+// MOSI = Data input (from pi to antenna)
+// pins 19 21 23 = mosi miso sclk
+
+
+
+public class RunRecieve {
+    public static void main(String[] args) throws InterruptedException {
+        GpioController gpio = GpioFactory.getInstance();
+
+        // defines the CE pin
+        GpioPinDigitalOutput CEpin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_06, "MyLED", PinState.HIGH);
+        GpioPinDigitalInput MISOPin = gpio.
+        GpioPinDigitalOutput MOSI = gpio
+
+
+
+//        while(true){
+//            System.out.println("Toggling LED");
+//            pin.toggle();
+//            Thread.sleep(1000);
+//        }
+    }
+}
