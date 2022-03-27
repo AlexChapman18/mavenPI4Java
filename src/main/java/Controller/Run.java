87fbeb227b8bfed06ffd0cbe44f60a088d30c7bc
Alexander Chapman <achapman5@sheffield.ac.uk>
2022-03-27 14:07:49 +0100

    changed to SpiDevice instead

--- src/main/java/Controller/Run.java
+++ src/main/java/Controller/Run.java
@@ -2,6 +2,8 @@ package Controller;
 
 import com.pi4j.io.gpio.*;
 import com.pi4j.io.spi.SpiChannel;
+import com.pi4j.io.spi.SpiDevice;
+import com.pi4j.io.spi.SpiFactory;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -22,25 +24,27 @@ public class Run {
         byte frequencyChannel = (byte) 0x05;
 
 //        Creates a spi object on channel cs0 (One connected on raspberry pi)
-        SpiClass receiver = new SpiClass(SpiChannel.CS0);
+        SpiDevice receiver = SpiFactory.getInstance(SpiChannel.CS0, // default spi speed 1 MHz
+                SpiDevice.DEFAULT_SPI_SPEED, // default spi mode 0
+                SpiDevice.DEFAULT_SPI_MODE);
 
 //        Set PRIM_RX bit to 1
-        System.out.println(Arrays.toString(receiver.spi.write(PRIM_RX, (byte) 0x0D)));
+        System.out.println(Arrays.toString(receiver.write(PRIM_RX, (byte) 0x0D)));
 
 //        Set Acknowledgement to 0
-        System.out.println(Arrays.toString(receiver.spi.write(Acknowledgement, (byte) 0x00)));
+        System.out.println(Arrays.toString(receiver.write(Acknowledgement, (byte) 0x00)));
 
 //        Use same address width
-        System.out.println(Arrays.toString(receiver.spi.write(AddressWidth, (byte) 0x03)));
+        System.out.println(Arrays.toString(receiver.write(AddressWidth, (byte) 0x03)));
 
 //        Use same frequency channel
-        System.out.println(Arrays.toString(receiver.spi.write(frequencyChannel, (byte) 0x01)));
+        System.out.println(Arrays.toString(receiver.write(frequencyChannel, (byte) 0x01)));
 
 //        Set PWR_UP and CE to high
-        System.out.println(Arrays.toString(receiver.spi.write(PRIM_RX, (byte) 0x0F)));
+        System.out.println(Arrays.toString(receiver.write(PRIM_RX, (byte) 0x0F)));
         CEpin.toggle();
 
-        System.out.println(Arrays.toString(receiver.spi.write((byte) 0x61)));
+        System.out.println(Arrays.toString(receiver.write((byte) 0x61)));
         System.out.println("Ended");
 //      System.out.println("Clock: " + Clock.getState() + ", Data: " + RX.getState());
 
